c888a7824c6be0f5c382b2070646e27fe6f2d3d4
rhksghss <rhksgh99@gmail.com>
2022-05-11 22:17:22 +0900

    2022.05.11

--- Baekjoon/Baekjoon.java
+++ Baekjoon/Baekjoon.java
@@ -1,5 +1,12 @@
 public class Baekjoon{
     public static void main(String[] args){
+        baekjoon_2557();
+        
+    }
+
+    public static void baekjoon_2557(){
         System.out.print("Hello World!");    //구현 2557번
     }
+
+
 }
