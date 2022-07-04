5b6f0362fbd604e90859519fdf28de3ea79f9566
dev-choee <rhksgh99@gmail.com>
2022-07-04 23:34:57 +0900

    dfs/bfs 어렵다..

---
+++ 01.OJ/2.Programmers/Python3/dfsbfs.py
@@ -0,0 +1,26 @@
+# 타겟 넘버
+# https://programmers.co.kr/learn/courses/30/lessons/43165
+'''
+1. 총합이 타겟넘버가 되는 조합을 찾는다.
+2. 
+3. 
+4. 
+5. 
+'''
+def dfs(0, 0, numbers, target):
+    stack=[] # 1.방믄여부
+    nodes=[] # 2.노드의 가지 리스트
+    sum=0
+    dfs(0,0,0,0)
+    dfs(0,0,0,0)
+
+
+
+def solution(numbers, target):
+    answer = 0
+    return answer
+
+
+
+
+
