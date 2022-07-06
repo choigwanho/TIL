9cb9bcbbe386f872f05a93d0f4f70def5262b82d
dev-choee <rhksgh99@gmail.com>
2022-07-06 23:05:34 +0900

    구현 문제 풂

--- 01.OJ/1.Baekjoon/Python3/0.구현/implementation.py
+++ 01.OJ/1.Baekjoon/Python3/0.구현/implementation.py
@@ -56,3 +56,93 @@ def sol1475():
     cnt[6]=(cnt[6]+1)//2
     print(max(cnt))
 
+# 2750 수 정렬하기
+# https://www.acmicpc.net/problem/2750
+'''
+1. 아이디어
+- 중복되지 않는 N개의 수를 오름차순으로 정렬한다.
+2. 복잡도
+- sort() 리스트 내장함수
+3. 자료구조
+- 크기 n 짜리 리스트
+*추가
+sorted()는 새로운 정렬된 리스트 반환
+sort는 기존 리스트에 정렬(in-place)
+참조: https://docs.python.org/ko/3/howto/sorting.html
+'''
+def sol2750():
+    n=int(input())
+    nums=list(int(input().rstrip()) for _ in range(n))
+
+    for n in sorted(nums):
+        print(n)
+
+# 10872 팩토리얼
+# https://www.acmicpc.net/problem/10872
+'''
+1. 아이디어
+- n!을 출력한다.
+- n!=n*(n-1)*(n-2)*...*1
+2. 복잡도
+- O(n)
+3. 자료구조
+- 
+'''
+def sol10872():
+    def f(n):
+        if n==0:
+            return 1
+        else:       
+            return n*f(n-1)
+    print(f(int(input())))
+
+# 2558 A+B - 2
+def sol2558():
+    a= int(input())
+    b =int(input())
+    print(a+b)
+    
+# 15596 정수 N개의 합
+# https://www.acmicpc.net/problem/15596
+def solve(a):
+    ans = sum(a)
+    return ans
+
+# 10817 세 수
+def sol10817():
+    nums =list(map(int,input().split()))
+    nums.sort()
+    print(nums[1])
+    
+# 2440 별 찍기 - 3
+def sol2440():
+    n = int(input())
+    for i in range(n,0,-1):
+        print("*"*i)
+    print()
+
+# 1316 그룹 단어 체커
+# https://www.acmicpc.net/problem/1316
+'''
+1. 아이디어
+- 다음 문자와 서로 다를 때, 그 뒤에 현재 문자가 있으면 그룹단어에서 제외
+2. 복잡도
+- len(word) 상수 복잡도
+3. 자료구조
+- 리스트
+'''
+def sol1316():
+    n = int(input())
+
+    for _ in range(n):
+        word = input().strip()
+
+        for i in range(len(word)-1):
+            if word[i]!=word[i+1]:
+                if word[i] in word[i+1:]:
+                    n-=1
+                    break
+    print(n)
+    
+    
+sol1316()
