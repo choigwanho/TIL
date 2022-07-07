e78192d3a0343e8879623aee36e445e4cccfe7cc
dev-choee <rhksgh99@gmail.com>
2022-07-07 22:45:49 +0900

    구현 3문제

--- 01.OJ/1.Baekjoon/Python3/0.구현/implementation.py
+++ 01.OJ/1.Baekjoon/Python3/0.구현/implementation.py
@@ -144,5 +144,72 @@ def sol1316():
                     break
     print(n)
     
+# 7287 등록
+def sol7287():
+    print(59)
+    print('rhksghsss')
+
+# 5622 다이얼
+'''
+1. 아이디어
+- 3으로 나누어 알파벳 그룹을 나누려고 보니, 7과 9는 4글자씩 있다.
+- 알파벳 리스트를 만들어서 직접 비교
+2. 복잡도
+- word의 길이 시간 복잡도
+3. 자료구조 
+- 리스트
+'''
+def sol5622():
+    alpabet_list = ['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
+    word = input()
+
+    time =0 
+    for unit in alpabet_list:
+        for i in unit:
+            for x in word:
+                if i == x:
+                    time += alpabet_list.index(unit) + 3
+
+    print(time)
+
+# 2941 크로아티아 알파벳
+def sol2941():
+    alpabet_list=['c=','c-','dz=','d-','lj','nj','s=','z=']
+    word=input().strip()
+    cnt=0
+
+    for alpabet in alpabet_list:
+        for i in range(len(word)-len(alpabet)+1):
+            if alpabet == word[i:i+len(alpabet)]:
+                cnt+=1
+        word = word.replace(alpabet," ")
+    word= word.replace(" ","")
+
+    print(len(word))
+    print(cnt+len(word))
+sol2941()
+            
+            
+
+
+        
+
+# 1996 프린터 큐
+# https://www.acmicpc.net/problem/1966
+# '''
+# 1. 아이디어
+# - 
+# - 
+# - 
+# 2. 복잡도
+# 3. 자료구조
+# '''
+# from collections import deque
+
+# def sol1966():
+#     tc = int(input())
+#     for i in range(tc):
+        
+
+
     
-sol1316()
