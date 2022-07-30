e8f2526813d0c1e7cbb23fa7ac4a0b7537054e89
dev-choee <rhksgh99@gmail.com>
2022-07-31 08:08:36 +0900

    2022.07.21

--- 01.OJ/1.Baekjoon/Python3/BOJ_15686.py
+++ 01.OJ/1.Baekjoon/Python3/BOJ_15686.py
@@ -3,6 +3,8 @@
 ### 문제 접근
 ```Python
 1. 아이디어
+- "치킨 거리" = 집과 가장 가까운 치킨집 사이의 거리
+- 각각의 집의 치킨거리를 구하여 거리가 먼 치킨집을 배제한다.
 -
 
 2. 복잡도
@@ -13,3 +15,12 @@
 ```
 '''
 ### 해결 코드
+
+import sys
+si = sys.stdin.readline
+
+N, M = map(int, input().split())
+city = [[map(int, input().split())] for _ in range(N)]
+
+
+
