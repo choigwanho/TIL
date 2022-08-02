a57bda0bc36655921b10a0f9cbeab8dc8b7e5c81
dev-choee <rhksgh99@gmail.com>
2022-08-02 10:51:14 +0900

    20227, 17836 해결중

---
+++ 01.OJ/BOJ_20207.py
@@ -0,0 +1,21 @@
+# [BOJ_20207 달력 -Python3](https://www.acmicpc.net/problem/20207)
+## 문제분석
+'''
+```Python
+1. 관찰
+-
+
+2. 복잡도
+-
+
+3. 자료구조
+-
+
+```
+'''
+## 해결코드
+import sys 
+si = sys.stdin.readline
+
+N = int(si())
+plan = [list(map(int, si().split())) for _ in range(N)]
