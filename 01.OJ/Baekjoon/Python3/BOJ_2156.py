8a6cd65b23e3b079674f36a973292fbc92a69e56
dev-choee <rhksgh99@gmail.com>
2022-08-14 12:14:34 +0900

    2022.08.14

--- 01.OJ/Baekjoon/Python3/BOJ_2156.py
+++ 01.OJ/Baekjoon/Python3/BOJ_2156.py
@@ -8,7 +8,11 @@
 - 포도주는 마시고 원위치
 - 연속으로 놓여 있는 3잔을 모두 마실 수 없다.
 
-- 
+- 가장 많은 포도주를 마시는 방법을 구한다.
+
+bottom up 방식으로 연산
+- 초기화 dp[0 for _ in range(n+1)]
+- 두 잔을 연속으로 마시는 경우 or 한 잔만 마시는 경우
 
 2. 복잡도
 - 
@@ -24,6 +28,17 @@
 import sys
 si = sys.stdin.readline
 
-n = int(si())
-wine = [int(si()) for _ in range(n)]
+N = int(si())
+wine = [0]
+for i in range(N):
+    wine.append(int(si()))
 
+dp = [0]
+dp.append(wine[1])
+if N>1: 
+    dp.append(wine[1]+wine[2]) 
+for n in range(3,N+1): 
+    dp.append(max(dp[n-1], dp[n-2]+wine[n], dp[n-3]+wine[n-1]+wine[n])) 
+print(dp[N])
+     
+    
