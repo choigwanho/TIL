c21c259c31908a9a7883bbe4ec83f905ab6a0d56
dev-choee <rhksgh99@gmail.com>
2022-10-17 17:47:36 +0900

    2022.10.17 크루스칼 학습

--- 01.OJ/Baekjoon/Python3/BOJ_9370.py
+++ 01.OJ/Baekjoon/Python3/BOJ_9370.py
@@ -56,64 +56,8 @@
 '''
 
 
-
-
-def getMinDistanceIndex():
-    min = INF
-    index = 0
-    for i in range(n):
-        if distance[i] < min and not visited[i]:
-            min = distance[i]
-            index = i
-    return index
-
-def dijkstra(start):
-    for i in range(n):
-        distance[i] = graph[start][i]
-    visited[start] = 1 # 방문처리
-    for i in range(n):
-        current = getMinDistanceIndex()
-        visited[current] = 1
-        for j in range(n):
-            if not visited[j]:
-                if distance[current] + graph[current][j] < distance[j]:
-                    distance[j] = distance[current] + graph[current][j]
-
-
-import heapq
-import sys
-si = sys.stdin.readline
-
-T = int(si())
-for _ in range(T):
-    n, m, t = map(int,si().split()) # 노드의 개수, 간선의 개수, 목적지의 개수
-    s, g, h = map(int,si().split()) # 출발 노드, 지나간 두 노드
-
-    # 전체 그래프 초기화
-    INF = float('inf') # 연결되지 않은 경우 비용 무한
-    graph = [[INF]*n for _ in range(n)] # 0(n) 2,000
-    for i in range(n): # 0(n) 2,000
-        graph[i][i] = 0
-    for _ in range(m): # 0(n) 50,000
-        a, b, d = map(int,si().split()) # 두 개의 노드와 그 사이 거리
-        graph[a-1][b-1] = d
-        graph[b-1][a-1] = d
-
-    # 목적지 리스트 초기화
-    target_list = [int(si()) for _ in range(t)] # 0(n) 1,000
-
-    visited = [0 for _ in range(n)]  # 0(n) 2,000
-    distance = [0 for _ in range(n)] # 0(n) 2,000
-
-    dijkstra(s-1)
-    print(distance)
-
-
-
-'''
-
 def dijkstraByAdjacencyList(graph, start):
-    distances = {node: float('inf') for node in graph}
+    distances = {node: int(1e9) for node in graph}
     distances[start] = 0
     pq = []
     heapq.heappush(pq, [distances[start], start])
@@ -157,11 +101,66 @@ for _ in range(T):
     s_distances = dijkstraByAdjacencyList(graph, s)
     answer = []
     for target in target_list:
-        print(isinstance(s_distances[target], float))
         if isinstance(s_distances[target], float):
             answer.append(target)
 
     print(*sorted(answer))
 
-  
-  
+'''
+
+def dijkstraByAdjacencyList(graph, start):
+    distances = {node: int(1e9) for node in graph}
+    distances[start] = 0
+    pq = []
+    heapq.heappush(pq, [distances[start], start])
+
+    while pq:
+        current_distance, current_target = heapq.heappop(pq)
+        if distances[current_target] < current_distance:
+            continue
+            
+        for new_target, new_distance in graph[current_target].items():
+            distance = current_distance + new_distance
+            if distance < distances[new_target]:
+                distances[new_target] = distance
+                heapq.heappush(pq, [distance, new_target])
+    return distances
+
+import heapq
+from collections import defaultdict
+import sys
+si = sys.stdin.readline
+
+T = int(si())
+for _ in range(T):
+    n, m, t = map(int,si().split()) # 노드의 개수, 간선의 개수, 목적지의 개수
+    s, g, h = map(int,si().split()) # 출발 노드, 지나간 두 노드
+
+    graph = defaultdict(dict)
+    for node in range(n+1): # 0(n) 2,000
+        graph[node]
+    for _ in range(m): # 0(n) 50,000
+        a, b, d = map(int,si().split()) # 두 개의 노드와 그 사이 거리
+        graph[a][b] = d
+        graph[b][a] = d
+
+    # 목적지 리스트 초기화
+    target_list = [int(si()) for _ in range(t)] # 0(n) 1,000
+
+    s_distances = dijkstraByAdjacencyList(graph, s)
+
+
+    gh_distance = graph[h][g]
+
+    # 중복된 이동을 제거하기 위해 필수 통로 제거
+    graph[g].pop(h)
+    graph[h].pop(g)
+
+    answer = []
+    for target in target_list:
+        target_distances = dijkstraByAdjacencyList(graph, target)
+        if s_distances[target] >=  min(s_distances[g] + gh_distance + target_distances[h], s_distances[h] + gh_distance + target_distances[g]): # s-g-h-목적지 거리가 최단거리보다 작거나 같으면 가능
+             answer.append(target)
+
+    print(*sorted(answer))
+
