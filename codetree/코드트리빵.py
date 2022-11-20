5f0f00bf850bd4e6bbbb0077a0c71f68950a452d
dev-choee <rhksgh99@gmail.com>
2022-11-20 21:08:29 +0900

    빵

--- codetree/코드트리빵.py
+++ codetree/코드트리빵.py
@@ -1,14 +1,9 @@
-import sys
-from collections import deque
-si = sys.stdin.readline
-
 def OOB(x,y):
     return x<0 or x>=N or y<0 or y>=N
 
-# 가장 가까운 베이스캠프를 리턴하는 함수1 (행,열 우선순위 - 상,좌,하,우 순으로 탐색 방식)
+# 가장 가까운 베이스캠프를 리턴하는 함수1 (행,열 우선순위 - 상,좌,우,하 순으로 탐색 방식 -> 그리디한 방법이지만 검증필요)
 def basecamp1(sx,sy):
     q = deque([(sx,sy)])
-    
     while q:
         x,y = q.popleft()
         for dir in range(4):
@@ -26,10 +21,8 @@ def basecamp1(sx,sy):
 def basecamp2(sx,sy):
     check = [[0]*N for _ in range(N)]
     camps = []
-
     check[sx][sy] = 1
     q = deque([(sx,sy)])
-    
     while q:
         x,y = q.popleft()
         for dir in range(4):
@@ -45,23 +38,25 @@ def basecamp2(sx,sy):
             check[nx][ny] = 1   
     return sorted(camps, key=lambda x: (x[0],x[1]))[0]
 
-def solve():
+def solve(): 
+    # 모든 사람이 편의점에 도착하는 시간
+    rst = 0
     for sx,sy in store:
         # store에서 가장 가까운 베이스캠프 찾기
         bx,by = basecamp1(sx-1,sy-1)
         # 찾은 베이스캠프 방문처리
         vis[bx][by]=1
 
-        # 
-       
-       
-       
-       
+        # 현재 움직이고 있는 사람들의 좌표
+    
         # print(bx,by)
         # for n in range(N):
         #     print(*vis[n])
         # print()
 
+import sys
+from collections import deque
+si = sys.stdin.readline
 
 # 격자의 크기, 사람의 수
 N,M = map(int,si().split())
@@ -69,14 +64,8 @@ N,M = map(int,si().split())
 block = [list(map(int,si().split())) for _ in range(N)]
 # 편의점의 위치 (행,열)
 store = [list(map(int,si().split())) for _ in range(M)]
-
 # 베이스캠프/편의점 방문처리
 vis = [[0]*N for _ in range(N)]
-
-dx,dy = [-1,0,1,0], [0,-1,0,1]
+dx,dy = [-1,0,0,1], [0,-1,1,0]
 
 solve()
-
-
-# 모든 사람이 편의점에 도착하는 시간
-rst = 0
